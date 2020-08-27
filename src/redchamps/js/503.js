bafe0c763a097464b83c1c5dd6fdb4e2a2f42c45
Rav [RedChamps] <rav@redchamps.com>
2020-08-27 13:22:59 +0530

    added feature to auto refresh maintenance page in 2 minutes

--- src/redchamps/js/503.js
+++ src/redchamps/js/503.js
@@ -29,3 +29,30 @@ elementsTL.forEach(element => {
 
     element.style.setProperty("transform-origin", resultTL)
 }); // forEach
+
+timer(
+    120, // seconds
+    function(timeleft) { // called every step to update the visible countdown
+        document.getElementById('timer').innerHTML = "Page will be automatically refreshed in "+timeleft+" minute(s)";
+    },
+    function() { // what to do after
+        document.getElementById('timer').innerHTML = "Page is being reloaded";
+        location.reload();
+    }
+);
+
+function timer(time,update,complete) {
+    var start = (new Date().getTime())/1000;
+    var interval = setInterval(function() {
+        var now = time-((new Date().getTime())/1000-start);
+        if( now <= 0) {
+            clearInterval(interval);
+            complete();
+        }
+        else {
+            var minutes = ('0'+Math.floor(now / 60)).slice(-2);
+            var seconds = ('0'+Math.floor(now - minutes * 60)).slice(-2);
+            update(minutes+':'+seconds);
+        }
+    },100); // the smaller this number, the more accurate the timer will be
+}
